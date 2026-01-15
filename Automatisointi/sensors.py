84af25d01156bedeeb65e769307328453d6620ca
TNuppi <teronik@metropolia.fi>
2026-01-15 08:49:28 +0000

    Removed import random, use_simulation

--- Automatisointi/sensors.py
+++ Automatisointi/sensors.py
@@ -1,8 +1,7 @@
 # sensors.py
 from robot_types import SensorData
-from robot_config import CAMERA_AVABLE, IMU_AVABLE, USE_SIMULATION, MODBUS_AVAILABLE
+from robot_config import CAMERA_AVABLE, IMU_AVABLE, IO_AVABLE, MODBUS_AVAILABLE
 from modbus_worker import modbus_worker
-import random
 import requests
 import logging
 
@@ -35,11 +34,18 @@ def safe_motor_voltage(motor_id):
     data = modbus_worker.get_status(motor_id)
     return data.get("voltage_V") if data else None
 
+def read_IO_data():
+    if not IO_AVABLE:
+        return 1, 0, 0, 0, 0
+    return 0, 0, 0, 0, 0  # TODO: oikea IO-luku
 def read_sensors() -> SensorData:
     motor1_freg = safe_motor_freq(1)
     motor3_freg = safe_motor_freq(3)
     motor4_freg = safe_motor_freq(4)
     motor6_freg = safe_motor_freq(6)
+
+    battery1_voltage = safe_motor_voltage(1)
+    battery2_voltage = safe_motor_voltage(4)
 
     cam_left, cam_center, cam_right = read_camera_depth() if CAMERA_AVABLE else (None, None, None)
     imu_x, imu_y, imu_z = read_IMU_heading() if IMU_AVABLE else (None, None, None)
@@ -49,10 +55,17 @@ def read_sensors() -> SensorData:
         motor3_measured_freq=motor3_freg,
         motor4_measured_freq=motor4_freg,
         motor6_measured_freq=motor6_freg,
+        battery1_voltage_V=battery1_voltage,
+        battery2_voltage_V=battery2_voltage,
         cam_measured_depth_left=cam_left,
         cam_measured_depth_center=cam_center,
         cam_measured_depth_right=cam_right,
         IMU_heading_x=imu_x,
         IMU_heading_y=imu_y,
         IMU_heading_z=imu_z,
+        IO_data_1=read_IO_data()[0],
+        IO_data_2=read_IO_data()[1],
+        IO_data_3=read_IO_data()[2],
+        IO_data_4=read_IO_data()[3],
+        IO_data_5=read_IO_data()[4],
     )

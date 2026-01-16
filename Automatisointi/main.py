a9ced5ebb3ab15eb914c022ddcda94a2efd10640
TNuppi <teronik@metropolia.fi>
2026-01-16 08:09:06 +0000

    päivitetty control looppia niine että lasketaan kuljettu matka

--- Automatisointi/main.py
+++ Automatisointi/main.py
@@ -10,16 +10,16 @@ from perception import perceive
 from decision import decide
 from control import apply_control, stop_all_motors
 from gui import start_gui
-from robot_config import CONTROL_LOOP_DT
+from robot_config import CONTROL_LOOP_DT, DEBUG_MAIN
 from modbus_worker import modbus_worker
-from state import update_perception
+from state import update_perception, calculate_time_delta, add_distance_travelled
 
 logging.basicConfig(
     
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s'
 )
 logger = logging.getLogger("MAIN")
-
+logger.setLevel(logging.DEBUG if DEBUG_MAIN else logging.INFO)
 stop_event = threading.Event()
 
 
@@ -32,12 +32,21 @@ def control_loop():
 
     while not stop_event.is_set():
         try:
+            # 1. Lue sensorit
             sensors = read_sensors()
+            # 2. käsittele sensori data ja tee havainnointi
             perception = perceive(sensors)
-
+            # 3. päivitä havainnointi tila
             update_perception(perception)
-
+            # 4. laske kulunut aika
+            dt = calculate_time_delta()
+            # 5. laske kuljettu matka
+            velocity = perception.measured_velocity or 0.0
+            distance = dt * velocity
+            add_distance_travelled(distance)
+            # 6. tee päätös
             command = decide(perception)
+            # 7. lähetä käsky moottoreille
             apply_control(command)
 
         except Exception:
